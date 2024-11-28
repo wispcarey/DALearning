2f3dae34e19a190625f64c6b2a126e05cb226708
Bohan Chen <bhchen@caltech.edu>
2024-11-27 18:38:33 -0800

    freeze WQ

--- scripts/run_enscorrection_train.sh
+++ scripts/run_enscorrection_train.sh
@@ -2,8 +2,41 @@
 
 cd ..
 
+# python learn_enscorrection_set_transformer_local.py \
+#     --epochs 50 \
+#     --dataset lorenz96 \
+#     --N 10 \
+#     --sigma_y 1 \
+#     --seed 42 \
+#     --adjust_lr
+
+# python learn_enscorrection_set_transformer_local.py \
+#     --epochs 50 \
+#     --dataset lorenz96 \
+#     --N 10 \
+#     --sigma_y 1 \
+#     --seed 42 \
+#     --adjust_lr \
+#     --unfreeze_WQ
+
+
 python learn_enscorrection_set_transformer_local.py \
-    --epochs 50 \
+    --dataset lorenz63 \
+    --N 10 \
+    --sigma_y 1 \
+    --seed 42 \
+    --no_localization \
+    --adjust_lr
+
+python learn_enscorrection_set_transformer_local.py \
+    --dataset lorenz63 \
+    --N 10 \
+    --sigma_y 0.7 \
+    --seed 42 \
+    --no_localization \
+    --adjust_lr
+
+python learn_enscorrection_set_transformer_local.py \
     --dataset lorenz96 \
     --N 10 \
     --sigma_y 1 \
@@ -11,57 +44,24 @@ python learn_enscorrection_set_transformer_local.py \
     --adjust_lr
 
 python learn_enscorrection_set_transformer_local.py \
-    --epochs 50 \
     --dataset lorenz96 \
+    --N 10 \
+    --sigma_y 0.7 \
+    --seed 42 \
+    --adjust_lr
+
+python learn_enscorrection_set_transformer_local.py \
+    --dataset ks \
     --N 10 \
     --sigma_y 1 \
     --seed 42 \
-    --adjust_lr \
-    --unfreeze_WQ
+    --adjust_lr
 
-
-# python learn_enscorrection_set_transformer_local.py \
-#     --dataset lorenz63 \
-#     --N 10 \
-#     --sigma_y 1 \
-#     --seed 42 \
-#     --no_localization \
-#     --adjust_lr
-
-# python learn_enscorrection_set_transformer_local.py \
-#     --dataset lorenz63 \
-#     --N 10 \
-#     --sigma_y 0.7 \
-#     --seed 42 \
-#     --no_localization \
-#     --adjust_lr
-
-# python learn_enscorrection_set_transformer_local.py \
-#     --dataset lorenz96 \
-#     --N 10 \
-#     --sigma_y 1 \
-#     --seed 42 \
-#     --adjust_lr
-
-# python learn_enscorrection_set_transformer_local.py \
-#     --dataset lorenz96 \
-#     --N 10 \
-#     --sigma_y 0.7 \
-#     --seed 42 \
-#     --adjust_lr
-
-# python learn_enscorrection_set_transformer_local.py \
-#     --dataset ks \
-#     --N 10 \
-#     --sigma_y 1 \
-#     --seed 42 \
-#     --adjust_lr
-
-# python learn_enscorrection_set_transformer_local.py \
-#     --dataset ks \
-#     --N 10 \
-#     --sigma_y 0.7 \
-#     --seed 42 \
-#     --adjust_lr
+python learn_enscorrection_set_transformer_local.py \
+    --dataset ks \
+    --N 10 \
+    --sigma_y 0.7 \
+    --seed 42 \
+    --adjust_lr
 
 
